0f01d7e7187ee31e3f474e2d9bfbe2195bb2898a
hxxisq <onlybenjii@gmail.com>
2025-10-22 17:38:48 +0100

    added interactive user input

--- position size calculator.py
+++ position size calculator.py
@@ -1,30 +1,42 @@
-# account_balance = 10000
-# risk_percent = 1
-# stop_loss_pips = 50
-#
-# risk_amount = account_balance * (risk_percent / 100)
-#
-# pip_value = risk_amount / stop_loss_pips
-#
-# mini_lot = pip_value / 1
-#
-# print("...Lots Size Calculator...")
-# print(f"Account Balance: ${account_balance}")
-# print(f"Risk Percent: {risk_percent}%")
-# print(f"Stop Loss Pips: {stop_loss_pips} pips")
-# print(f"\nRisk Amount: ${risk_amount:,.2f}")
-# print(f"Mini lots size: {mini_lot:,.2f} lots")
-
 def calculate_lot_size (balance, risk_percent, stop_loss):
     risk_amount = balance * (risk_percent/100)
     pip_value = risk_amount / stop_loss
     mini_lots = pip_value / 1
     return{
-        "risk amount": risk_amount,
-        "mini lots": mini_lots,
+        "risk_amount": risk_amount,
+        "mini_lots": round(mini_lots, 2)
     }
 
-print("10k Account with 1% Risk\n")
-result = calculate_lot_size(balance=10000, risk_percent=1, stop_loss=50)
-print(f"Risk: ${result['risk amount']:.2f}")
-print(f"Mini Lots: ${result['mini lots']:.2f}")
+def get_number_input(user_input):
+    while True:
+        try:
+            value = float(input(user_input))
+            if value <= 0:
+                print("Enter a positive number!")
+                continue #this restarts the loop
+            return value
+        except ValueError:
+            print("Invalid input! please enter a number!")
+
+def main():
+    print('POSITION SIZE CALCULATOR')
+
+    while True:
+        #get user input
+        account_balance = get_number_input("\nAccount balance($): ")
+        risk_percent = get_number_input("Risk percent(%): ")
+        stop_loss = get_number_input("Stop loss(pips): ")
+
+        #calculate lots size
+        result = calculate_lot_size(account_balance, risk_percent, stop_loss)
+
+        #display result
+        print(f"Risk: ${result['risk_amount']}\nMini lot: {result['mini_lots']}")
+
+        #ask if user wants to calculate again
+        repeat = input("\nWant to calculate again? (y/n) ").lower()
+        if repeat != "y":
+            break
+
+if __name__ == '__main__':
+    main()
